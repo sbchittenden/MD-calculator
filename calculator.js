5f60e51ef7eb8f13dc0fca5c535a2cd5efd9311a
Sam Chittenden <sbchittenden@gmail.com>
2016-09-09 22:52:47 -0500

    Updated calcultor.js and index and css
    Added blinking cursor functions

--- calculator.js
+++ calculator.js
@@ -4,23 +4,7 @@
     //calculator.js //
     //////////////////
 
-    /* ------------------------------------------------------------------------
-    This Project Assignment will add functionality to the calculator you created in the HTML & CSS chapter projects.
 
-    It will function by building a string based on the buttons pressed. Each button press should append the button’s value to the “screen” text input, with the exception of the CLEAR, =, and DELbuttons. Their functionality is specified below:
-
-    The CLEAR button should clear the text input of all characters.
-
-    The DEL button should remove the last character at the end of the text input.
-
-    The = button should do the following:
-
-    - Place the contents of the “screen” text input into a variable.
-
-    - Run the built-in function eval on the variable. More on eval below.
-
-    - Set the “screen” text input to be the result of the eval call.
-    --------------------------------------------------------------------------*/
 
     ///////////////////////////////////////////
     // retrieve calculator elements from DOM //
@@ -70,6 +54,16 @@
         console.log(calculatorDisplay);
     }
 
+    ///////////////////////////////////////////
+    // function to check for blinking cursor //
+    ///////////////////////////////////////////
+
+    function blinkRemove() {
+        if (calculatorDisplay.innerHTML === '<span id="cursor">_</span>') {
+            calculatorDisplay.innerHTML = '';
+        }
+    }
+
     //////////////////////////////////////////
     // add event listeners for key presses //
     /////////////////////////////////////////
@@ -77,73 +71,90 @@
     /* ------------ decimal key ------------------ */
 
     decimalKey.addEventListener('click', function() {
+    	blinkRemove();
         appendInput('.');
     });
 
     /* ------------ number keys ------------------ */
 
     zeroKey.addEventListener('click', function() {
+        blinkRemove();
         appendInput('0');
     });
 
     oneKey.addEventListener('click', function() {
+        blinkRemove();
         appendInput('1');
     });
 
     twoKey.addEventListener('click', function() {
+        blinkRemove();
         appendInput('2');
     });
 
     threeKey.addEventListener('click', function() {
+        blinkRemove();
         appendInput('3');
     });
 
     fourKey.addEventListener('click', function() {
+        blinkRemove();
         appendInput('4');
     });
 
     fiveKey.addEventListener('click', function() {
+        blinkRemove();
         appendInput('5');
     });
 
     sixKey.addEventListener('click', function() {
+        blinkRemove();
         appendInput('6');
     });
 
     sevenKey.addEventListener('click', function() {
+        blinkRemove();
         appendInput('7');
     });
 
     eightKey.addEventListener('click', function() {
+        blinkRemove();
         appendInput('8');
     });
 
     nineKey.addEventListener('click', function() {
+        blinkRemove();
         appendInput('9');
     });
 
     /* ------------ operator keys ------------- */
 
     addKey.addEventListener('click', function() {
+    	blinkRemove();
         appendInput('+');
     });
 
     subKey.addEventListener('click', function() {
+    	blinkRemove();
         appendInput('-');
     });
 
     multiKey.addEventListener('click', function() {
+    	blinkRemove();
         appendInput('*');
     });
 
     divideKey.addEventListener('click', function() {
+    	blinkRemove();
         appendInput('/');
     });
 
     /* ----------- clear all key --------------- */
 
     allClearKey.addEventListener('click', function() {
-        calculatorDisplay.innerHTML = "";
+        if (calculatorDisplay.innerHTML !== '<span id="cursor">_</span>') {
+            calculatorDisplay.innerHTML = '<span id="cursor">_</span>';
+        }
     });
 
     /* ----------- delete key --------------- */
@@ -153,13 +164,13 @@
         calculatorDisplay.innerHTML = deleted;
     });
 
-/* ----------- equals key --------------- */
+    /* ----------- equals key --------------- */
 
-equalsKey.addEventListener('click', function(){
-	var calcDisplayString = calculatorDisplay.innerHTML;
-	var evaluatedInput = eval(calcDisplayString);
-	calculatorDisplay.innerHTML = evaluatedInput;
-});
+    equalsKey.addEventListener('click', function() {
+        var calcDisplayString = calculatorDisplay.innerHTML;
+        var evaluatedInput = eval(calcDisplayString).toFixed(5);
+        calculatorDisplay.innerHTML = evaluatedInput;
+    });
 
 
 
